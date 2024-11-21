51370608860079bd23b05bd471ef877a40defbe0
BabaFatimaZahra <fz.baba22@gmail.com>
2024-11-22 00:05:40 +0100

    updated

--- src/components/UpdateNote.jsx
+++ src/components/UpdateNote.jsx
@@ -6,7 +6,7 @@ function UpdateNote() {
      
     const navigate = useNavigate();
 
-    const [noteData, setNoteData] = useState({title: '', content: ''});
+    const [noteData, setNoteData] = useState({});
     // const [title, setTitle] = useState('');
     // const [content, setContent] = useState('');
     const {id} = useParams();
@@ -25,17 +25,21 @@ function UpdateNote() {
     const setNote = async (e) => {
         e.preventDefault();
         const url = `https://notes.devlop.tech/api/notes/${id}`;
-        const resp = await axios.put(url, {title: noteData.title, content: noteData.content});
+        const resp = await axios.put(url, noteData);
         // console.log(resp);
         navigate('/');
+    }
+
+    const handleChange = (e) => {   
+        setNoteData({...noteData, [e.target.name]: e.target.value});
     }
 
     return (
         <>
             <h1 id="note">Update Note</h1>
             <form action="" id='create-note' >
-                <input type="text" name='title' value={noteData.title} onChange={e => setNoteData({...noteData, title: e.target.value})}  placeholder="Title" required/> <br />
-                <textarea name='content' value={noteData.content} onChange={e => setNoteData({...noteData, content: e.target.value})}  placeholder=" Content ..." maxLength={200} required></textarea> <br />
+                <input type="text" name='title' value={noteData.title} onChange={handleChange}  placeholder="Title" required/> <br />
+                <textarea name='content' value={noteData.content} onChange={handleChange}  placeholder=" Content ..." maxLength={200} required></textarea> <br />
                 <button id="update-note" onClick={setNote}>Update</button>
             </form>
         </>
